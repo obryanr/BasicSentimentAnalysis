7eda771d2f6c8dff4167a79de20b434263195e82
obryanr <64580248+obryanr@users.noreply.github.com>
2021-08-28 00:04:01 +0700

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -274,3 +274,34 @@ class NLPVectorizer:
         
         elif method == 'chi2':
             pass
+
+'''FAST TEXT IMPORT'''
+
+def get_average_vec(tokens_list, vector, generate_missing=False, k=300):
+    """
+        Calculate average embedding value of sentence from each word vector
+    """
+    if len(tokens_list)<1:
+        return np.zeros(k)
+    
+    if generate_missing:
+        vectorized = [vector[word] if word in vector else np.random.rand(k) for word in tokens_list]
+    else:
+        vectorized = [vector[word] if word in vector else np.zeros(k) for word in tokens_list]
+    
+    length = len(vectorized)
+    summed = np.sum(vectorized, axis=0)
+    averaged = np.divide(summed, length)
+    return averaged
+    
+def get_embeddings(vectors, text, generate_missing=False, k=300):
+    """
+        create the sentence embedding
+    """
+    embeddings = text.apply(lambda x: get_average_vec(x, vectors, generate_missing=generate_missing, k=k))
+    return list(embeddings)
+
+def fast_text(fasttext_path):
+  from gensim.models.fasttext import FastText
+  fasttext_model = gensim.models.KeyedVectors.load_word2vec_format(fasttext_path, binary=False, limit=100000)
+  return fasttext_model
